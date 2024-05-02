d089fa7d3ee14647816247a3abc1446c897d3b00
izumi <zumtaurus23@gmail.com>
2024-05-02 14:00:13 +0900

    確認終了

--- createWhisperTables.sql
+++ createWhisperTables.sql
@@ -3,14 +3,14 @@
 
 /*---------------以下テーブル削除----------------------*/
 
-DROP TABLE user;
-DROP TABLE follow;
-DROP TABLE Whisper;
-DROP TABLE goodInfo;
-DROP TABLE followCntView;
-DROP TABLE followerCntView;
-DROP TABLE whisperCntView;
-DROP TABLE goodCntView;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS follow;
+DROP TABLE IF EXISTS Whisper;
+DROP TABLE IF EXISTS goodInfo;
+DROP TABLE IF EXISTS followCntView;
+DROP TABLE IF EXISTS followerCntView;
+DROP TABLE IF EXISTS whisperCntView;
+DROP TABLE IF EXISTS goodCntView;
 
 /*---------------以下テーブル作成----------------------*/
 
@@ -27,29 +27,49 @@ CREATE TABLE user(
 CREATE TABLE follow(
 	userId        VARCHAR(30),
 	followUserId  VARCHAR(30),
-	PRIMARY KEY(userId,followUserId)
+	PRIMARY KEY(userId,followUserId),
+	FOREIGN KEY ( userId ) REFERENCES user( userId ),
+	FOREIGN KEY ( followUserId ) REFERENCES user( userId )
 );
 
 
 -- ささやき管理表
 CREATE TABLE whisper(
-	whisperNo   BIGINT PRIMARY KEY AUTO_INCREMENT,
-	userId      VARCHAR(30)  NOT NULL
-	postDate    date         NOT NULL DEFAULT CURRENT_TIMESTAMP,	
-	content     VARCHAR(256) NOT NULL,
-	imagePath   VARCHAR(100)
+	whisperNo	BIGINT PRIMARY KEY AUTO_INCREMENT,
+	userId		VARCHAR(30)  NOT NULL,
+	postDate	DATE NOT NULL,
+	content		VARCHAR(256) NOT NULL,
+	imagePath	VARCHAR(100),
+	FOREIGN KEY ( userId ) REFERENCES user( userId )
 );
 
 -- イイね情報表
 CREATE TABLE goodInfo(
 	userId     VARCHAR(30),
 	whisperNo  BIGINT,
-	PRIMARY KEY(userId,whisperNo)
+	PRIMARY KEY(userId,whisperNo),
+	FOREIGN KEY ( userId ) REFERENCES user( userId ),
+	FOREIGN KEY ( whisperNo ) REFERENCES whisper( whisperNo )
 );
 
 -- フォロー件数ビュー
-CREATE followCntView(
-);
+CREATE view followCntView AS
+SELECT userId, COUNT(*) AS cnt FROM follow
+GROUP BY userId;
 
+-- フォロワー件数ビュー
+CREATE view followerCntView AS
+SELECT followUserId, COUNT(*) AS cnt FROM follow
+GROUP BY userId;
+
+-- ささやき件数ビュー
+CREATE view whisperCntView AS
+SELECT userId, COUNT(*) AS cnt FROM whisper
+GROUP BY userId;
+
+-- イイね件数ビュー
+CREATE view goodCntView AS
+SELECT whisperNo, COUNT(*) AS cnt FROM goodInfo
+GROUP BY whisperNo;
 
 
