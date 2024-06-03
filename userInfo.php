f2c8189be305e957ba2073b1da8c0f2a370370c9
izumi <zumtaurus23@gmail.com>
2024-06-03 16:25:30 +0900

    ユーザ情報取得　完成しました

--- userInfo.php
+++ userInfo.php
@@ -11,11 +11,17 @@ $response = [
     "result" => "",         // 実行結果を格納する(success or error)
     "errCode" => null,      // エラーコードがある場合格納する
     "errMsg" => null,       //エラーメッセージがある場合格納する
+    "userName" => "",
+    "profile" => "",
+    "icon" => "",
+
 ];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') { // HTTPメソッドがPOST形式で送られてきたか確認。
     $postData = json_decode(file_get_contents('php://input'), true);
 }
+
+
 
 /*
 １．Inputパラメータの必須チェックを行う。
@@ -23,68 +29,73 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') { // HTTPメソッドがPOST形式で
 　　【エラーコード】ユーザID：006
 */
 
-//inputパラメータの代入
-$userId = $postData["userId"];
 
-if (!isset($postData['userId'])) {
+if (!isset($postData['userId']) || $postData['userId'] == "") {
     $response = setError($response, "006");
-} else {
+}
+
+
+if (is_null($response["errCode"])) { // エラーじゃなければ
     // ２．DB接続処理を呼び出し、データベースの接続を行う。
     include('mysqlConnect.php');
-}
+    //inputパラメータの代入
+    $userId = $postData["userId"];
 
 
-/*
-３．ユーザ情報を取得するSQL文を実行する。
-対象列：ユーザ名、プロフィール、アイコン(userName,profile,icon)
-対象テーブル：ユーザ情報(user)
-取得対象：inputパラメータのユーザIDと一致するデータ
-*/
+    /*
+    ３．ユーザ情報を取得するSQL文を実行する。
+    対象列：ユーザ名、プロフィール、アイコン(userName,profile,icon)
+    対象テーブル：ユーザ情報(user)
+    取得対象：inputパラメータのユーザIDと一致するデータ
+    */
 
-// クエリ(SQL)の実行
-$sql = "SELECT userName,profile,icon FROM user WHERE userID = :userId";
-$stmt = $pdo->prepare($sql);
+    // クエリ(SQL)の実行
+    $sql = "SELECT userName,profile,iconPath FROM user WHERE userID = :userId";
+    $stmt = $pdo->prepare($sql);
 
-// データのバインド
-$stmt->bindParam(':userId', $userId, PDO::PARAM_STR);
+    // データのバインド
+    $stmt->bindParam(':userId', $userId, PDO::PARAM_STR);
 
-/*
-４．データのフェッチを行う。
-　　データが存在しない場合、対象エラーメッセージをセットしてエラー終了させる。
-　　【エラーコード】004
-*/
+    /*
+    ４．データのフェッチを行う。
+    　　データが存在しない場合、対象エラーメッセージをセットしてエラー終了させる。
+    　　【エラーコード】004
+    */
 
-if ($stmt->execute()) {
-    // 成功の処理
-    $response["result"] = "success";
-    // データの取得
-    // fetch()で1行ずつデータを取得。
-    while ($row = $stmt->fetch()) {
-        // fetch()で取得した1行分のデータが$rowに格納されているので、対象の列を指定して処理する。
-        // 大文字と小文字を区別されてたらしい。なぜ。
-        //返したときに表示する$dataをここで用意する。　それぞれの箱に$rowを詰める。
-        $data["userName"] = $row["userName"];
-        $data["profile"] = $row["profile"];
-        $data["icon"] =  $row["iconPath"];
-        // //レスポンスの枠組み(箱)の中に取得したデータを詰め込む
-        $response["list"][] = $data;
-        //var_dump($product_no);
+    $stmt->execute();
+
+    // クエリ結果の取得
+    $result = $stmt->fetchAll();
+
+    if (!empty($result)) {
+
+        //fetchで取得
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            //レスポンスの枠組み(箱)の中に取得したデータを詰め込む
+            // それぞれの箱に$rowを詰める。
+
+            // ５．返却値の連想配列に成功パラメータとユーザ情報のデータを格納する
+            $response["userName"] = $row["userName"];
+            $response["profile"] = $row["profile"];
+            $response["icon"] =  $row["iconPath"];
+        }
+
+        // 成功の処理
+        $response["result"] = "success";
+    } else {
+
+        //失敗の処理
+        $response = setError($response, "004");
     }
-} else {
-    //失敗の処理
-    $response = setError($response, "004");
+
+
+    // ６．SQL情報をクローズさせる。
+    $stmt = null;
+
+    // ７．DB切断処理を呼び出し、データベースの接続を解除する。
+    include('mysqlClose.php');
 }
 
-
-// ５．返却値の連想配列に成功パラメータとユーザ情報のデータを格納する
-
-
-
-// ６．SQL情報をクローズさせる。
-$stmt = null;
-
-// ７．DB切断処理を呼び出し、データベースの接続を解除する。
-include('mysqlClose.php');
 
 // ８．返却値の連想配列をJSONにエンコードしてoutputパラメータを出力する。
 header('Content-Type: application/json'); // JSON形式でレスポンスを送信するよう指定
