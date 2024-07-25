b4776adff67e267b3ce7375391591a4161e83447
izumi <zumtaurus23@gmail.com>
2024-07-25 11:21:15 +0900

    begintransactionを追加。配列に入れる処理を修正、エラーを格納する処理を変更しました。

--- followCtl.php
+++ followCtl.php
@@ -1,8 +1,8 @@
 <?php
-include('errorMsgs.php');		                            // エラーメッセージ用のPHPファイルの読み込み
+include('errorMsgs.php');                                    // エラーメッセージ用のPHPファイルの読み込み
 
-$response =[
-    
+$response = [
+
     "result" => "",                                         // 実行結果を格納する(success or error)
     "errCode" => null,                                      // エラーコードがある場合格納する
     "errMsg" => null,                                       // エラーメッセージがある場合格納する
@@ -10,26 +10,25 @@ $response =[
 ];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {                // HTTPメソッドがPOST形式で送られてきたか確認。
-	
-    $postData = json_decode(file_get_contents('php://input'), true);
 
+    $postData = json_decode(file_get_contents('php://input'), true);
 }
 
 // Inputパラメータの必須チェックを行う
-if(!isset($postData["userId"]) || $postData["userId"] == ""){
-    $response = setError($response,"006");                  // 【エラーコード】ユーザID：006
+if (!isset($postData["userId"]) || $postData["userId"] == "") {
+    $response = setError($response, "006");                  // 【エラーコード】ユーザID：006
 }
 
-if(!isset($postData["followUserId"]) || $postData["followUserId"] == ""){
-    $response = setError($response,"012");                  // 【エラーコード】ユーザID：012
+if (!isset($postData["followUserId"]) || $postData["followUserId"] == "") {
+    $response = setError($response, "012");                  // 【エラーコード】フォロユーザID：012
 }
 
-if(!isset($postData["followFlg"]) || !is_bool($postData["followFlg"])){
-    $response = setError($response,"013");                  // 【エラーコード】ユーザID：013
+if (!isset($postData["followFlg"]) || !is_bool($postData["followFlg"])) {
+    $response = setError($response, "013");                  // 【エラーコード】フォローフラグ指定なし：013
 }
 
 // エラーがなければ
-if($response["errCode"] == null){
+if ($response["errCode"] == null) {
 
     $userId = $postData["userId"];
     $followUserId = $postData["followUserId"];
@@ -37,36 +36,44 @@ if($response["errCode"] == null){
 
     include('mysqlConnect.php');                            // DB接続処理を呼び出し
 
+
+    //トランザクションの開始　-　泉
+    $pdo->beginTransaction();
+
+
     // フォローフラグがtrue(フォローする)の場合
-    if($followFlg == true){
+    if ($followFlg == true) {
         $sql = "INSERT INTO follow(userId,followUserId) VALUES(:userId,:followUserId)";
     }
 
     // フォローフラグがfalse(フォロー外す)の場合
-    if($followFlg == false){
+    if ($followFlg == false) {
         $sql = "DELETE FROM follow WHERE userId = :userId AND followUserId = :followUserId";
     }
 
     $stmt = $pdo->prepare($sql);
     // データのバインド
     $stmt->bindParam(':userId', $userId, PDO::PARAM_STR);
-    $stmt->bindParam(':followUserId',$followUserId,PDO::PARAM_STR);
+    $stmt->bindParam(':followUserId', $followUserId, PDO::PARAM_STR);
 
-    if($stmt->execute()){
-		// 成功の処理
-		$response = array('success' => true);
-	}else{
-		$response['errCode'] = '001';
-	}
+    if ($stmt->execute()) {
+        // 成功の処理   修正　-　泉
+        $response["result"] = "success";
+    } else {
+        // 失敗の処理　　修正　-　泉
+        // DB処理異常終了
+        // $response['errCode'] = '001';
+        $response = setError($response, "001");
+    }
 
-    $pdo ->commit();
+    $pdo->commit();
 
-	$response = array('success' => true);                  // 返却値の連想配列に成功パラメータをセット
+    //削除　-　泉
+    // $response = array('success' => true);                  // 返却値の連想配列に成功パラメータをセット
 
     $stmt = null;                                          // SQL情報をクローズ
 
     include('mysqlClose.php');
-}															
+}
 header('Content-Type: application/json'); // JSON形式でレスポンスを送信するよう指定
 echo json_encode($response, JSON_UNESCAPED_UNICODE); // $responseのデータをJSON形式に加工して出力
-?>
